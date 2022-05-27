f783262cbea141ec15e59bfd2f66b7b8338fe50b
Enes Yaldiz <Enes.Yaldiz@futavis.de>
2022-05-27 16:25:45 +0200

    adding-config-stuff

--- pomBotMain.py
+++ pomBotMain.py
@@ -3,14 +3,18 @@ from Jobs.pomBotSendJob import PomBotSendJob
 from pomBotSendMessage import PomBotSend
 from pomBotReceiveMessage import PomBotReceive
 from pomBotReactEmojis import PomBotReactEmojis
-
+from Config.pom_config import PomConfig
 
 try:
+    pomDuratingInMin = 25
+    pomBreakInMin = 5
+    pomStartMin = 30
+    myConfig = PomConfig.
     pomBotSend = PomBotSend()
     pomBotReceive = PomBotReceive()
     pomBotReact = PomBotReactEmojis()
-    pomBotSendJob = PomBotSendJob(pomBotSend.startEmojiToMogli, pomBotSend.doneEmojiToMogli,25,5)
-    pomBotReactJob = PomBotReactEmojisJob(25,5,pomBotReceive.getLastMessageIDMogli,pomBotReact.react_with_all_mogli)
+    pomBotSendJob = PomBotSendJob(pomBotSend.startEmojiToMogli, pomBotSend.doneEmojiToMogli,pomDuratingInMin,pomBreakInMin, pomStartMin)
+    pomBotReactJob = PomBotReactEmojisJob(pomDuratingInMin,pomBreakInMin,pomBotReceive.getLastMessageIDMogli,pomBotReact.react_with_all_mogli, pomStartMin)
     pomBotSendJob.start_cycle()
     pomBotReactJob.start_cycle()
 except Exception as e:
