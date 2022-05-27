4f8c3c64bc8e22c1292654fa6f4d247f7b69b3c9
Enes Yaldiz <Enes.Yaldiz@futavis.de>
2022-05-27 16:18:52 +0200

    fixed-timing

--- Jobs/pomBotReactEmojisJob.py
+++ Jobs/pomBotReactEmojisJob.py
@@ -4,13 +4,14 @@ from typing import Callable
 import threading
 
 class PomBotReactEmojisJob:
-    def __init__(self, pomDurationInMin: int, pomBreakTimeInMin: int, pomReceiveFunction:Callable, pomReactFunction:Callable):
+    def __init__(self, pomDurationInMin: int, pomBreakTimeInMin: int, pomReceiveFunction:Callable, pomReactFunction:Callable, pomStartMin:int):
         self._cycle_thread: threading.Thread = None
         self.stop: bool = False
         self.pomReceiveFunction = pomReceiveFunction
         self.pomReactFunction = pomReactFunction
         self.pomDurationInMin = pomDurationInMin
         self.pomBreakTimeInMin = pomBreakTimeInMin
+        self.pomStartMin = pomStartMin
 
     def start_cycle(self):
         self._cycle_thread = threading.Thread(target=self._cycle)
@@ -25,14 +26,14 @@ class PomBotReactEmojisJob:
         print('start cycle for react job')
         while not self.stop:
             now = datetime.datetime.now()
-            if (now.minute % (self.pomDurationInMin + self.pomBreakTimeInMin)) == 0:
+            if now.minute == self.pomStartMin:
                 time.sleep(2)
                 id = self.pomReceiveFunction()
                 self.pomReactFunction(id)
-                time.sleep(self.pomDurationInMin*50)
-            if (now.minute == 25) or (now.minute == 55):
+                time.sleep((self.pomDurationInMin*60)-(60-now.second))
                 time.sleep(2)
                 id = self.pomReceiveFunction()
                 self.pomReactFunction(id)
-                time.sleep(self.pomBreakTimeInMin*50)
+                time.sleep((self.pomBreakTimeInMin*60)-(60-now.second))
+                self.pomStartMin = now.minute
             time.sleep(1)
--- Jobs/pomBotSendJob.py
+++ Jobs/pomBotSendJob.py
@@ -5,13 +5,14 @@ import threading
 
 
 class PomBotSendJob:
-    def __init__(self, pomStartFunction: Callable, pomEndFunction: Callable, pomDurationInMin: int, pomBreakTimeInMin: int):
+    def __init__(self, pomStartFunction: Callable, pomEndFunction: Callable, pomDurationInMin: int, pomBreakTimeInMin: int, pomStartMin: int):
         self._cycle_thread: threading.Thread = None
         self.stop: bool = False
         self.pomStartFunction = pomStartFunction
         self.pomEndFunction = pomEndFunction
         self.pomDurationInMin = pomDurationInMin
         self.pomBreakTimeInMin = pomBreakTimeInMin
+        self.pomStartMin = pomStartMin
 
     def start_cycle(self):
         self._cycle_thread = threading.Thread(target=self._cycle)
@@ -26,11 +27,11 @@ class PomBotSendJob:
         print('start cycle for send job')
         while not self.stop:
             now = datetime.datetime.now()
-            if (now.minute % (self.pomDurationInMin + self.pomBreakTimeInMin)) == 0:
+            if now.minute == self.pomStartMin:
                 self.pomStartFunction()
-                time.sleep(self.pomDurationInMin*50)
-            if (now.minute == 25) or (now.minute == 55):
+                time.sleep((self.pomDurationInMin*60)-(60-now.second))
                 self.pomEndFunction()
-                time.sleep(self.pomBreakTimeInMin*50)
+                time.sleep((self.pomBreakTimeInMin*60)-(60-now.second))
+                self.pomStartMin = now.minute
             time.sleep(1)
 
