d4d6465e0cc0ae91a9c172bc5028b0657bc2054f
Enes Yaldiz <Enes.Yaldiz@futavis.de>
2022-11-21 15:53:29 +0100

    * idk

--- Enums/pomBotEnums.py
+++ Enums/pomBotEnums.py
@@ -11,7 +11,7 @@ class Payloads(ExtendedEnum):
     PAYLOAD_POM_START_1 = "@here pom start"
     PAYLOAD_POM_DONE_1 = "@here pom done"
     PAYLOAD_POM_START_EMOJI = "@here <a:echat_sparkles:794309945414778881> <:p_letter_p:795783313242980372> <:p_letter_o:795783310608957450> <:p_letter_m:795783310483783691>     <:p_letter_s:795783310206828548> <:p_letter_t:795783310563213363> <:p_letter_a:795783477269233724> <:p_letter_r:795783310630191104> <:p_letter_t:795783310563213363> <a:echat_sparkles:794309945414778881>"
-    PAYLOAD_POM_DONE_EMOJI = "@here <a:SquirrelRolli:665618752322273280>  <:p_letter_p:795783313242980372> <:p_letter_o:795783310608957450> <:p_letter_m:795783310483783691>     <:p_letter_d:795783310365294682> <:p_letter_o:795783310608957450> <:p_letter_n:795783310315880469> <:p_letter_e:795783310583791677>  <a:SquirrelRolli:665618752322273280> **rate the pom:**"
+    PAYLOAD_POM_DONE_EMOJI = "@here <a:SquirrelRolli:665618752322273280>  <:p_letter_p:795783313242980372> <:p_letter_o:795783310608957450> <:p_letter_m:795783310483783691>     <:p_letter_d:795783310365294682> <:p_letter_o:795783310608957450> <:p_letter_n:795783310315880469> <:p_letter_e:795783310583791677>  <a:SquirrelRolli:665618752322273280> "
 
 
 class ReactEmojisSparkles(ExtendedEnum):
--- pomBotMain.py
+++ pomBotMain.py
@@ -5,20 +5,20 @@ import utils
 if __name__ == "__main__":
     # set the one to the value you want and the other to 999
     pomStartMin = 333  # if you wanna start with a pom START
-    pomEndMin = 19  # if you wanna start with a pom end
+    pomEndMin = 50  # if you wanna start with a pom end
 
-    channel_id = 756232819440746586  # mowgli dm-group
+    channel_id = 1044050144917737522  # mowgli dm-group
     channel_string = utils.get_channel_string(channel_id)
 
-    pomDurationInMin = 5
-    pomBreakTimeInMin = 1
+    pomDurationInMin = 50
+    pomBreakTimeInMin = 10
 
     pomBotSendJob = SendMessageJob(channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin)
-    pomBotReactJob = ReactEmojiJob(channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin)
+    #pomBotReactJob = ReactEmojiJob(channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin)
     try:
         pomBotSendJob.start_cycle()
-        pomBotReactJob.start_cycle()
+        #pomBotReactJob.start_cycle()
     except Exception as e:
         print("exception has occured")
         pomBotSendJob.stop_cycle()
-        pomBotReactJob.stop_cycle()
+        #pomBotReactJob.stop_cycle()
