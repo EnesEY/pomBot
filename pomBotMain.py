9f414c31cb1c8d0699c71711ce14cb361128325e
Enes Yaldiz <Enes.Yaldiz@futavis.de>
2023-03-05 12:28:25 +0100

    * mark own message as unread

--- pomBotMain.py
+++ pomBotMain.py
@@ -5,9 +5,9 @@ import utils
 if __name__ == "__main__":
     # set the one to the value you want and the other to 999
     pomStartMin = 30  # if you wanna start with a pom START
-    pomEndMin = 333  # if you wanna start with a pom end
+    pomEndMin = 55  # if you wanna start with a pom end
 
-    channel_id = 1069868788629115020
+    channel_id = 1081888593557082123
     channel_string = utils.get_channel_string(channel_id)
 
     pomDurationInMin = 25
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     pomBotSendJob = SendMessageJob(
         channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin
     )
-    # pomBotReactJob = ReactEmojiJob(channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin)
+    pomBotReactJob = ReactEmojiJob(channel_string, pomStartMin, pomEndMin, pomDurationInMin, pomBreakTimeInMin)
     try:
         pomBotSendJob.start_cycle()
-        # pomBotReactJob.start_cycle()
+        pomBotReactJob.start_cycle()
     except Exception as e:
         print("exception has occured")
         pomBotSendJob.stop_cycle()
-        # pomBotReactJob.stop_cycle()
+        pomBotReactJob.stop_cycle()
